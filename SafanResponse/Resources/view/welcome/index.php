a1ff2fa61b7a1c1843530323e68ed9bcde55c366
haroutweb <haroutweb@gmail.com>
2014-11-01 16:04:37 -0400

    Welcome page

--- SafanResponse/Resources/view/welcome/index.php
+++ SafanResponse/Resources/view/welcome/index.php
@@ -1,5 +1,5 @@
 <h2 class="f10">Welcome to Safan-framework</h2>
 <p>
     It's simple PHP framework created by <a href="https://github.com/safan-lab" class="cBlue f08 linked">Safan Lab</a>.<br/>
-    For a detailed study of the first read the documentation on <a href="https://github.com/safan-lab/safan-framework-standart" class="cBlue linked">Github</a>.
+    For a detailed study of the first read the documentation on <a href="https://github.com/safan-lab/safan-framework-standard" class="cBlue linked">Github</a>.
 </p>
